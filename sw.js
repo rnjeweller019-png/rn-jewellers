6a28124ac5a9a681f1d1a915d56b712bb211c97a
Naitik Dhiman <naitikdhiman06@gmail.com>
2026-08-29 21:19:21 +0530

    fix: add OneSignalSDKWorker.js and importScripts in sw.js for mobile Service Worker push handling

--- sw.js
+++ sw.js
@@ -1,4 +1,6 @@
-const CACHE_NAME = 'rnj-jewellers-v26';
+importScripts('https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js');
+
+const CACHE_NAME = 'rnj-jewellers-v27';
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
